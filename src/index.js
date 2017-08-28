115c6a89ff96974946f5e342812a05832895e2c0
wk645 <wk645@nyu.edu>
2017-08-27 23:28:55 -0400

    code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
 // Code goes here
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div className="movie-card">
+        <img  
+          src={this.props.poster} 
+          alt={this.props.title} 
+        />
+        <h2>{this.props.title}</h2>
+        <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+    );
+  }
+}
+
+MovieCard.defaultProps = {
+	poster: 'http://i.imgur.com/bJw8ndW.png'
+};
+
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+ 
+ReactDOM.render(
+  <MovieCard
+    title="Jurassic World"
+    genres={jurassicWorldGenres} 
+  />,
+  document.getElementById('root')
+);
